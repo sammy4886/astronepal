7df20c1905a51d61431e9dcd7df20926e58fec37
Shalabh Adhikari <shalabh@accessworld.net>
2018-02-08 19:53:26 +0545

    contact form

--- resources/views/frontend/partials/contact.blade.php
+++ resources/views/frontend/partials/contact.blade.php
@@ -56,6 +56,52 @@
             </div>
         </div>
     </div>
+    <div class="ast_mapnform_wrapper ast_toppadder70">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-8 col-sm-10 col-xs-12 col-lg-offset-2 col-md-offset-2 col-sm-offset-1 col-xs-offset-0">
+                    <div class="ast_heading">
+                        <h1>find & message <span>here</span></h1>
+                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected hummer.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="ast_contact_map">
+            <div class="col-lg-6 col-md-8 col-sm-8 col-xs-12 col-lg-offset-3 col-md-offset-2 col-sm-offset-2 col-xs-offset-0">
+                <div class="ast_contact_form">
+                    <form>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>first name</label>
+                            <input type="text" name="first_name" class="require">
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>last name</label>
+                            <input type="text" name="last_name" class="require">
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>email</label>
+                            <input type="text" name="email" class="require" data-valid="email" data-error="Email should be valid.">
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>subject</label>
+                            <input type="text" name="subject" class="require">
+                        </div>
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <label>message</label>
+                            <textarea rows="5" name="message" class="require"></textarea>
+                        </div>
+                        <div class="response"></div>
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <button class="ast_btn pull-right submitForm" type="button" form-type="contact">send</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3504.665591193351!2d-81.23677798444672!3d28.54976958245048!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e7666b70c7639b%3A0x138d66cd5d424720!2sBloomfield+Dr%2C+Orlando%2C+FL+32825%2C+USA!5e0!3m2!1sen!2sin!4v1501822735922" allowfullscreen></iframe>
+        </div>
+    </div>
+
 
 
 @endsection
