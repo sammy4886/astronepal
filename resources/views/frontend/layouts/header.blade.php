beaeb6c3dd8fa332b3183d6497125dd58f99781a
Shalabh Adhikari <shalabh@accessworld.net>
2018-03-06 17:23:46 +0545

    logo

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -46,7 +46,7 @@
         <div class="row">
             <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
                 <div class="ast_logo">
-                    <a href="{{url('/')}}"><img src="images/header/logo.png" alt="Logo" title="Logo"></a>
+                    <a href="{{url('/')}}"><img src="images/astromandu.png" alt="Logo" title="Logo"></a>
                     <button class="ast_menu_btn"><i class="fa fa-bars" aria-hidden="true"></i></button>
                 </div>
             </div>
